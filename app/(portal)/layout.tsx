521d796d034c1edcc364d9bd07f4809b9d22f121
Odendaal Oosthuizen <it@carenetconsultants.co.za>
2026-06-08 10:42:04 +0200

    Remove sidebar from portal layout

--- app/(portal)/layout.tsx
+++ app/(portal)/layout.tsx
@@ -1,14 +1,7 @@
-import { Sidebar } from '@/components/layout/Sidebar'
-
 export default function PortalLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return (
-    <div className="min-h-screen bg-cnc-gray-50">
-      <Sidebar />
-      <div className="ml-64 min-h-screen flex flex-col">{children}</div>
-    </div>
-  )
+  return <>{children}</>;
 }

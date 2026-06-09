dd8bcbaf017be7f72ffdc4ff4ceba44cf5bec6a2
Cassandra Kruger <Cassandra@carenetconsultants.co.za>
2026-06-09 11:53:55 +0200

    Update layout.tsx

--- app/(portal)/layout.tsx
+++ app/(portal)/layout.tsx
@@ -1,7 +1,21 @@
+import { Sidebar } from '@/components/layout/Sidebar'
+import { Header } from '@/components/layout/Header'
+
 export default function PortalLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  return <>{children}</>;
+  return (
+    <div className="min-h-screen bg-cnc-gray-50">
+      {/* Fixed black sidebar (264px wide) */}
+      <Sidebar />
+
+      {/* Everything sits to the right of the sidebar */}
+      <div className="pl-64 min-h-screen flex flex-col">
+        <Header title="Dashboard" />
+        <main className="flex-1 p-6">{children}</main>
+      </div>
+    </div>
+  )
 }

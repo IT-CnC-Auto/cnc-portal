1f5ef85c83354ef59d1cd883b0fe0814919a37c6
Cassandra Kruger <Cassandra@carenetconsultants.co.za>
2026-06-11 11:33:16 +0200

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// The approved CNC portal design is served as a static file from /public/portal.html.
-// We send the site root straight there instead of the old /dashboard scaffold.
-export default function Home() {
-  redirect('/home.html')
-}
